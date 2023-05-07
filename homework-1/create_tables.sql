1a56e26d468ed1eb96f2867dce4a1b00ca14bfd4
Nikkon97 <you@example.com>
2023-05-07 20:29:35 +0300

    init commit

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(50) PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50) REFERENCES customers(customer_id),
+	employee_id int2 NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
